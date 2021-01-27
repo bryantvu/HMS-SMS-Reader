849ea30f2ad6e2cce94e325f7098fe690e60a84c
Bryant Vu <bryantvu@gmail.com>
2021-01-26 16:47:37 -0800

    added log print out in MyBroadcastReceiver.kt

--- app/src/main/java/com/bvutest/retrievingsmscode/MyBroadcastReceiver.kt
+++ app/src/main/java/com/bvutest/retrievingsmscode/MyBroadcastReceiver.kt
@@ -16,16 +16,37 @@ class MyBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
 
+        Log.d(TAG, "onReceive")
+
         val bundle = intent!!.extras
         if (bundle != null) {
+            Log.d(TAG, "onReceive >> bundle != null")
             val status: Status? = bundle.getParcelable(ReadSmsConstant.EXTRA_STATUS)
             if (status?.statusCode == CommonStatusCodes.TIMEOUT) {
 
                 // Service has timed out and no SMS message that meets the requirement is read. Service ended.
                 // doSomethingWhenTimeOut()
+                Log.d(TAG, "onReceive >> CommonStatusCodes.TIMEOUT")
+                if (bundle.containsKey(ReadSmsConstant.EXTRA_SMS_MESSAGE)) {
+                    Log.d(TAG, "onReceive >> bundle contains ReadSmsConstant.EXTRA_SMS_MESSAGE")
+
+                    // An SMS message that meets the requirement is read. Service ended.
+                    //doSomethingWhenGetMessage(bundle.getString(ReadSmsConstant.EXTRA_SMS_MESSAGE))
+
+                    bundle.getString(ReadSmsConstant.EXTRA_SMS_MESSAGE)?.let {
+
+                        Log.d(TAG, it)
+
+                        val local = Intent()
+                        local.action = "service.to.activity.transfer"
+                        local.putExtra("sms", it)
+                        context!!.sendBroadcast(local)
+                    }
+                }
             } else if (status?.statusCode == CommonStatusCodes.SUCCESS) {
 
                 if (bundle.containsKey(ReadSmsConstant.EXTRA_SMS_MESSAGE)) {
+                    Log.d(TAG, "onReceive >> bundle contains ReadSmsConstant.EXTRA_SMS_MESSAGE")
 
                     // An SMS message that meets the requirement is read. Service ended.
                     //doSomethingWhenGetMessage(bundle.getString(ReadSmsConstant.EXTRA_SMS_MESSAGE))
@@ -41,6 +62,8 @@ class MyBroadcastReceiver : BroadcastReceiver() {
                     }
                 }
             }
+        }else{
+            Log.d(TAG, "onReceive >> bundle == null")
         }
     }
 }
